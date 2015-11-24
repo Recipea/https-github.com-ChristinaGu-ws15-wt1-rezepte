06c20df1829a4cfbd5ce828af97879889564fd14
unknown <Nadine@.(none)>
2015-11-24 13:44:07 +0100

    setup for search

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
   def index
+    
+    if params[:search]
+      @recipes = Recipe.search(params[:search]).order("created_at DESC")
+    else
+      @recipes = Recipe.order("created_at DESC")
+    end
+
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,5 +3,14 @@ class Recipe < ActiveRecord::Base
   belongs_to :category
   has_and_belongs_to_many :user
   has_and_belongs_to_many :tag, :join_table => :tags_and_recipes
+  
+  attr_accessible :name
+  validates :name, presence: true
+  # It returns the recipes whose titles contain one or more words that form the query
+  def self.search(query)
+    # where(:title, query) -> This would return an exact match of the query
+    where("title like ?", "%#{query}%") 
+  end  
+  
 end
 
